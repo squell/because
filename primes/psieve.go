d9c33d58b0d571c5f3eafb053c037395dc44d49f
Marc Schoolderman <marc.schoolderman@xs4all.nl>
2015-09-25 00:13:48 +0200

    cleanup

--- primes/psieve.go
+++ primes/psieve.go
@@ -7,26 +7,30 @@ import "fmt"
 const MAX_PRIME = 25000
 
 func sieve(in <-chan int, out chan<- int) {
-    myPrime := <-in
-    out <- myPrime
-    filt := make(chan int)
-    go sieve(filt, out)
-    for n := range in {
-        if(n % myPrime != 0) {
-            filt <- n
-        }
-    }
+	myPrime := <-in
+	out <- myPrime
+	filt := make(chan int)
+	go sieve(filt, out)
+	for n := range in {
+		if n%myPrime != 0 {
+			filt <- n
+		}
+	}
 }
 
 func main() {
-    outp := make(chan int)
-    inp := make(chan int)
-    go sieve(inp, outp)
-    go func() {
-        for x:=2; ; x++ { inp <- x }
-    }()
-    for n := range outp {
-        if(n>=MAX_PRIME) { break }
-        fmt.Println(n)
-    }
+	outp := make(chan int)
+	inp := make(chan int)
+	go sieve(inp, outp)
+	go func() {
+		for x := 2; ; x++ {
+			inp <- x
+		}
+	}()
+	for n := range outp {
+		if n >= MAX_PRIME {
+			break
+		}
+		fmt.Println(n)
+	}
 }
