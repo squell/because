2e4520036cd156e3093160a09c3dfdb611fbcf25
Marc Schoolderman <marc.schoolderman@xs4all.nl>
2016-02-04 19:03:05 +0100

    dining philosophers in go

---
+++ philosophers/dining.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+func Lefty(id int, eating chan int, left chan int, right chan int) {
+	for {
+		y := <-left
+		x := <-right
+		eating <- id
+		right <- x
+		left <- y
+	}
+}
+
+func Philosopher(id int, eating chan int, left chan int, right chan int) {
+	for {
+		x := <-right
+		y := <-left
+		eating <- id
+		right <- x
+		left <- y
+	}
+}
+
+func Table(id int, fork chan int) {
+	for {
+		fork <- id
+		<-fork
+	}
+}
+
+func main() {
+	var forks [5]chan int
+	for i := range forks {
+		forks[i] = make(chan int)
+		go Table(i, forks[i])
+	}
+
+	var eating chan int
+	eating = make(chan int)
+	for i := range forks {
+		if i == 0 {
+			go Lefty(i, eating, forks[i], forks[(i+1)%5])
+		} else {
+			go Philosopher(i, eating, forks[i], forks[(i+1)%5])
+		}
+	}
+
+	for {
+		fmt.Printf("philosopher #%d eating\n", <-eating)
+	}
+}
